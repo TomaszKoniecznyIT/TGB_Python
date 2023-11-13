89f66b73d9c6e00ace26dc7758e0804759447f20
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-13 18:28:04 +0000

    connection to the database, adding a user

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
+
+CREATE_USERS_TABLE = ("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, login VARCHAR (255) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL,  manager BOOLEAN DEFAULT True)")
+
+INSERT_NEW_USER = ("INSERT INTO users(login, password, manager) VALUES(%s, %s, %s)")
 
 load_dotenv()
 
@@ -13,3 +17,17 @@ connection = psycopg2.connect(url)
 @app.get("/users")
 def get_users():
     return "Hello"
+
+@app.post("/users/signup")
+def add_user():
+    data = request.get_json()
+    login = data['login']
+    password = data['password']
+    manager = data['manager']
+
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_USERS_TABLE)
+            cursor.execute(INSERT_NEW_USER, (login, password, manager))
+
+    return {"message": f'User {login} added.'}, 201
