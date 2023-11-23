15614ffc8fd8c6f440cff3835e34eb87369593a0
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-23 10:25:14 +0000

    adding a check to see if such a user already exists

--- app.py
+++ app.py
@@ -26,20 +26,45 @@ def get_user(email):
     
     return user
 
+def get_user_id(email):
+    user_id = get_user(email)
+    if (user_id):
+        return user_id[0]
+    return False
+    
 
 @app.post("/users/signup")
 def add_user():
     data = request.get_json()
     email = data['email']
     password = data['password']
+    # The try block is implemented to safeguard against the absence of a value for 'manager,' just as it is currently (only email and password can be passed to the database). However, if in the future we wish to pass a value for 'manager' as well, it is already implemented.
     try:
         manager = data['manager']
     except KeyError:
         manager = True
 
+    user_id = get_user_id(email)
+    if user_id:
+        return {"message": f'User {email} already exists.'}, 409
+    else:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(CREATE_USERS_TABLE)
+                cursor.execute(INSERT_NEW_USER, (email, password, manager))
+
+        return {"message": f'User {email} added.'}, 201
+     
+
+
+@app.post("/users/login")
+def login():
+    data =  request.get_json()
+    email = data['email']
+    password = data['password']
+
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(CREATE_USERS_TABLE)
-            cursor.execute(INSERT_NEW_USER, (email, password, manager))
+            pass
 
-    return {"message": f'User {email} added.'}, 201
+    return {"message": "You are logged in." }, 200
